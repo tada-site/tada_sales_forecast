f3d5489472cc6ffac5b18dc347dc13ff57ef4604
aleksandr-ol <oleksandr.oleniuk@gmail.com>
2025-01-02 21:03:55 +0200

    add search for model in external DB

--- app/api/views.py
+++ app/api/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from . import models
 
 # Create your views here.
+def test(request):
+
+    if request.method == "POST":
+        searched = request.POST['searched']
+        departments = models.Departments.objects.filter(name__icontains=searched)
+        return render (request, "test.html", 
+                       {"departments": departments, "searched": searched})
+    else:
+        departments = models.Departments.objects.all()
+        return render (request, "test.html", {"departments": departments})
+
+
+def goods(request):
+    departments = models.Departments.objects.all()
+    return render (request, "departments.html", {"departments": departments})
+
+
+def categories(request):
+    departments = models.Departments.objects.all()
+    return render (request, "departments.html", {"departments": departments})
--- app/app/router.py
+++ app/app/router.py
@@ -1,24 +1,22 @@
 
 class ApiRouter:
-    api_db = "tada_api"
+    api_db = "test_db"
     default_db = "default"
 
     def db_for_read(self, model, **hints):
-        model_name = model._meta.model_name
-        if model_name == 'goods':
+        if model._meta.app_label == 'api':
             return self.api_db
         else:
             return None
 
     def db_for_write(self, model, **hints):
-        model_name = model._meta.model_name
-        if model_name == 'goods':
+        if model._meta.app_label == 'api':
             return self.api_db
         else:
             return None
 
     def allow_migrate(self, db, app_label, model_name=None, **hints):
-        if model_name == 'goods':
+        if app_label == 'api':
             return self.api_db
         else:
             return db == 'default'
