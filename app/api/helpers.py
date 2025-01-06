0aae773acc6f945614200b6932773184cbe8d221
aleksandr-ol <oleksandr.oleniuk@gmail.com>
2025-01-06 18:49:13 +0200

    logs

--- app/api/helpers.py
+++ app/api/helpers.py
@@ -122,7 +122,10 @@ def get_good_graph_data(good_code, group_by=GROUP_BY_DAY, is_category=False):
     if len(res) == 0:
         return False
 
+    print(res)
     res = transpone(res)
+    print("transpone")
+    print(res)
     return return_graph(res[1], res[0])
 
 
